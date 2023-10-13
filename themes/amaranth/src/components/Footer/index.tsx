f807d22eb18e6c21fe2c6f92196e0e13240cd762
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-10-13 12:25:23 -0400

    delete userLinks

--- themes/amaranth/src/components/Footer/index.tsx
+++ themes/amaranth/src/components/Footer/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import UserLinks from "../UserLinks";
 import { Caption } from "../../theme";
 
 import * as S from "./styles";
 
 const Footer = (): JSX.Element => (
-    <S.Wrapper>
-      <S.LinkGrid>       
-        <UserLinks includeRss />
-      </S.LinkGrid>
+    <S.Wrapper>      
       <S.Info>        
         <Caption style={{fontSize:'14px'}}>Diablo4.Tips is a fanmade site sharing content about Diablo 4, utilizing official game media solely for educational purposes, with no ownership or claim on the intellectual property</Caption>
       </S.Info>
